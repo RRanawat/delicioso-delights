062575d3d043a94fcb92a9722c0512f4e137ec85
RRanawat <ranawatrivaan@gmail.com>
2024-12-07 19:29:49 +0000

    displaying images based on ckae name

--- script.js
+++ script.js
@@ -25,13 +25,26 @@ function nextStep() {
 window.onload = function () {
     const localStorageOrderDetails = JSON.parse(localStorage.getItem("order-details"));
     console.log(window.location);
-    if(!(localStorageOrderDetails || window.location.pathname == "/order.html")) {
+    if (!(localStorageOrderDetails || window.location.pathname == "/order.html")) {
         window.location = "/";
         return;
     }
     for (const number of [1, 2, 3, 4, 5, 6]) {
         const elem = document.getElementById(`review-step-${number}`);
         elem.innerText = localStorageOrderDetails[`step-${number}`].replaceAll("-", " ");
+        if (number == 2) {
+            const cakeName = localStorageOrderDetails[`step-2`];
+            const img = document.getElementById("review-cake-image");
+            if (cakeName == 'carrot-cake') {
+                img.src = "/assets/images/carrot.png";
+            } else if (cakeName == 'red-velvet') {
+                img.src = "/assets/images/red.png";
+            } else if (cakeName == 'chocolate') {
+                img.src = "/assets/images/chocolate.png";
+            } else if (cakeName == 'vanilla') {
+                img.src = "/assets/images/vanilla.png";
+            }
+        }
     }
 }
 

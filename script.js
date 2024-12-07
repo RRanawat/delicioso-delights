1520885d6c2e07dd8563872eac2570c53c19bae5
RRanawat <ranawatrivaan@gmail.com>
2024-12-07 18:59:42 +0000

    added review order page with local storage

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ const orderDetails = {};
 
 function nextStep() {
     const currentStepElement = document.getElementById(`step-${currentStep}`);
-    
+
     const inputElement = currentStepElement.querySelector('select, textarea, input');
-    
+
     if (inputElement && inputElement.value.trim() === '') {
         alert('This is required information!');
         return;
     }
 
     orderDetails[`step-${currentStep}`] = inputElement.value.trim();
-    
+
     currentStepElement.classList.remove('active');
     currentStep++;
     const nextStepElement = document.getElementById(`step-${currentStep}`);
     if (nextStepElement) {
         nextStepElement.classList.add('active');
     }
-    console.log(orderDetails);
+    localStorage.setItem("order-details", JSON.stringify(orderDetails));
+}
+
+window.onload = function () {
+    const localStorageOrderDetails = JSON.parse(localStorage.getItem("order-details"));
+    for(const number of [1,2,3,4,5,6]) {
+        const elem = document.getElementById(`review-step-${number}`);
+        elem.innerText = localStorageOrderDetails[`step-${number}`].replaceAll("-", " ");
+    }
 }
